aa4e1e77aa16ffb789d8e3fc571c6363b594aca2
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 15:54:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_reader :id, :name, :breed
 
-  def initialize(:name, :breed, :id = nil)
+  def initialize(name:, breed:, :id = nil)
     @id = id
     @name = name
     @breed = breed
