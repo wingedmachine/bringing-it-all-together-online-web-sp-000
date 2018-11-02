0662ba7fdf8d0d1d918b9491602d3db2710628af
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 16:06:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,7 @@ class Dog
     SQL
 
     row = DB[:conn].execute(find, id).first
-    Dog.new({ name: row[1], breed: [row[2], id: row[0] })
+    Dog.new({ name: row[1], breed: row[2], id: row[0] })
   end
 
   def self.create_table
